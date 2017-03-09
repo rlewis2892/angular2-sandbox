0b1522d4fe47b7ba761070b0dfd345dc1376cc7c
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-09 16:08:30 -0700

    create component, route, template for google-map

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,7 +1,8 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
+import {MapComponent} from "./components/map-component";
 
-export const allAppComponents = [HomeComponent];
+export const allAppComponents = [HomeComponent, MapComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent}
---
+++ app/components/map-component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: "google-map",
+	templateUrl: "./templates/map.php"
+})
+
+export class MapComponent {
+	title: string = "It's a Google Map";
+	lat: number = 51.678418;
+	lng: number = 7.809007;
+}
