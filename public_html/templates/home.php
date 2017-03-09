0fa5a90557f315d9893304814db3e2ab32ec69cb
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-09 14:42:38 -0700

    add base /public_html files

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1><i class="fa fa-bolt"></i> Welcome to the Angular 2 Sandbox!</h1>
+			<p class="lead">This is where we destroy your front end. :D</p>
+		</div>
+	</div>
+</section>
