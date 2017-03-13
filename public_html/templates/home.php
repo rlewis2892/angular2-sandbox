32bad6f895e4eafbadf1e4f52dac00f045a5da07
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-13 15:02:29 -0600

    add search field, css to template

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,6 +1,9 @@
 <header class="navbar-fixed-top">
 	<div class="container">
 		<h1>Angular Google Map Demo</h1>
+		<div class="form-group">
+			<input placeholder="search for location" autocorrect="off" autocapitalize="off" spellcheck="off" type="text" class="form-control" #search [formControl]="searchControl">
+		</div>
 	</div>
 </header>
 
